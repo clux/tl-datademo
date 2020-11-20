e26d1dc531c6d0114f87c01aae5f8ada94d05d98
clux <sszynrae@gmail.com>
2020-11-20 16:50:31 +0000

    get callback to work

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ Create a `.env` file with the following contents:
 ```sh
 export CLIENT_ID=sandbox-myid-xxxx
 export CLIENT_SECRET=xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx
+export CALLBACK_URI=https://public_url_of_self/signin_callback
+# data-api specific
+export PROVIDERS="providers=uk-ob-all uk-oauth-all uk-cs-mock"
+export SCOPE="info accounts transactions"
 ```
 
 Then source it and start the app:
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,66 @@
 use serde::Deserialize;
-use anyhow::{Context, Result};
 use log::{info};
 
 use actix_web::http::{header, Method, StatusCode};
 use actix_web::{
-    error, get, guard, middleware, web, App, Error, HttpRequest, HttpResponse,
-    HttpServer,
+    error, get, guard, middleware, web::{self, Data, Query}, App, Error, HttpRequest, HttpResponse,
+    HttpServer, Result,
 };
 #[derive(Deserialize, Debug, Clone)]
 struct Config {
     client_id: String,
     client_secret: String,
+    callback_uri: String,
     providers: String,
+    scope: String,
+}
+
+impl Config {
+    fn auth_link(&self) -> anyhow::Result<url::Url> {
+        let base_url = "https://auth.truelayer-sandbox.com/?".to_string();
+        let mut url = url::Url::parse(&base_url)?;
+        let qp = format!("response_type=code&client_id={id}&redirect_uri={redir}&scope={s}&providers={p}",
+            id=&self.client_id,
+            redir=&self.callback_uri,
+            s=&self.scope,
+            p=&self.providers,
+        );
+        url.set_query(Some(&qp));
+        Ok(url)
+    }
+}
+
+struct Credentials {
+    code: String,
+}
+
+impl Credentials {
+    fn from_code(code: String) -> Self {
+        Self { code }
+    }
 }
 
 #[get("/")]
-async fn index() -> &'static str {
-    "Hello world!\r\n"
+async fn index(cfg: Data<Config>) -> HttpResponse {
+    let url = cfg.auth_link().expect("invalid config");
+    let r = format!("Plz <a href=\"{}\" target=\"_blank\">bank</a>", url);
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(r)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct AuthResponse {
+   code: String,
+   scope: Option<String>,
+}
+
+#[get("/signin_callback")]
+async fn signin_callback(cfg: Data<Config>, Query(info): Query<AuthResponse>) -> HttpResponse {
+    info!("got cb with {:?}", info);
+    HttpResponse::build(StatusCode::OK)
+            .content_type("text/html; charset=utf-8")
+            .body(info.code)
 }
 
 #[actix_web::main]
@@ -33,6 +77,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .data(config.clone())
             .service(index)
+            .service(signin_callback)
     })
     .bind("0.0.0.0:5000")?
     .workers(1)
